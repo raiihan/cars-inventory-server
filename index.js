6b805f55b4fb8e96ddef0af57bf2ba0497e201a7
silvaan <s.siilvan@yahoo.com>
2022-04-30 21:21:16 +0600

    Deleted api create

--- index.js
+++ index.js
@@ -62,6 +62,14 @@ async function run() {
             const result = await productCollection.updateOne(filter, updateDoc, options);
             res.send(result);
         })
+
+        // delete api
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally { }
 }
